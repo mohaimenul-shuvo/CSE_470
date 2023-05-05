3bfef62a56e39146a403a464a71575f66d506e83
mohaimenul-shuvo <130241560+mohaimenul-shuvo@users.noreply.github.com>
2023-05-05 21:57:33 +0600

    Unit testing

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Database\Seeders\UsersTableSeeder;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+    //Check if login page exists
+    public function test_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    //Check if logout page exists
+    public function test_logout_form()
+    {
+        $response = $this->get('/logout');
+
+        $response->assertStatus(200);
+    }
+    //Test if a user can be deleted (make sure that you add the middleware)
+    public function test_delete_user()
+    {
+        $user = User::factory()->count(1)->make();
+
+        $user = User::first();
+
+        if($user) {
+            $user->delete();
+        }
+
+        $this->assertTrue(true);
+    }
+
+    //Perform a post() request to add a new user
+    public function test_if_it_stores_new_users()
+    {
+        $response = $this->post('/register', [
+            'name' => 'Dary',
+            'email' => 'dary@gmail.com',
+            'password' => 'dary1234',
+            'password_confirmation' => 'dary1234'
+        ]);
+
+        $response->assertRedirect('/home');
+    }
+
+    public function test_if_data_exists_in_database()
+    {
+        $this->assertDatabaseHas('users', [
+            'name' => 'Dary'
+        ]);
+    }
+
+    public function test_if_data_does_not_exists_in_database()
+    {
+        $this->assertDatabaseHas('users', [
+            'name' => 'shuvo'
+        ]);
+    }
+
+    public function test_if_seeders_works()
+    {
+        $this->seed();
+    }
+
+    public function test_if_seeder_works()
+    {
+        $this->seed(UsersTableSeeder::class);
+    }
+}
